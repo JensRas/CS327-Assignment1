ebb1930abfb22346bae6086e9142ab6583bf79d2
MythicAlchemist <thmsmccy@gmail.com>
2021-02-10 22:01:05 -0600

    debugging  failures

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all: assignment-1.01
-	gcc -Wall -Werror -ggdb3 assignment-1.01
+	make assignment-1.01
 
 assignment-1.01: assignment-1.01.c
 	gcc -Wall -Werror -ggdb3 assignment-1.01.c -o assignment-1.01
--- assignment-1.01.c
+++ assignment-1.01.c
@@ -6,7 +6,6 @@
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
-#include <math.h>
 
 #define minRoomNumber 6
 #define maxRoomNumber 10
@@ -21,9 +20,6 @@
 #define rockChar '.'
 #define upChar '<'
 #define downChar '>'
-
-
-// At least one cell of non-room 1 cell of non-room between any two different rooms
 
 // 0 = Immutable
 // 1 = Soft :)
@@ -48,7 +44,7 @@ void placeBorder(struct tiles(*floor)[floorMaxX][floorMaxY]);
 
 int main(int argc, char *argv[])
 {
-    srand(time(0)); // Random
+    srand(5); // Seed / Random
     int roomsWanted = (rand() % ((maxRoomNumber + 1) - minRoomNumber)) + minRoomNumber; // Always min , potential of adding rooms up to max  
 
     struct tiles floor[floorMaxX][floorMaxY];
@@ -56,37 +52,47 @@ int main(int argc, char *argv[])
 
     placeBorder(&floor);
     roomGen(&floor, &roomList, roomsWanted);
-    corridorGen(&floor, &roomList, roomsWanted);
-    staircaseGen(&floor, &roomList, roomsWanted);
-    
+    //corridorGen(&floor, &roomList, roomsWanted);
+    //staircaseGen(&floor, &roomList, roomsWanted);
     // Bugs with rooms only on the left third of the floor having pieces taken out around specific areas of the floor
     // Bugs with border having pieces of the left third of the floor
-    //placeBorder(&floor); // Covers up border problems
+    //placeBorder(&floor); // Kind of covers up problems
     printGame(&floor);
     return 0;
 }
 
 void roomGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomList)[maxRoomNumber], int roomsWanted)
 {
-    //srand(time(0)); // Random
     int i, j, k, failCount = 0;
     bool placedAll = false;
+    int temp1, temp2;
 
     while(!placedAll){
         placedAll = true;
-        for(i = 1; i < floorMaxX - 1; i++) { // Set everything to rock
+        for(i = 1; i < floorMaxX - 1; i++) { // Set everything (not border) to rock
             for (j = 1; j < floorMaxY - 1; j++) {
                 (*floor)[i][j].type = rockChar;
             }
         }
+        /* for(i = 0; i < roomsWanted; i++){
+            roomList[i]->sizeX = 0;
+            roomList[i]->sizeY = 0;
+            roomList[i]->cornerX = 0;
+            roomList[i]->cornerY = 0;
+        } */
 
         for(i = 0; i < roomsWanted; i++){
-            roomList[i]->sizeX = rand() % (maxRoomSize - minRoomX) + minRoomX;
-            roomList[i]->sizeY = rand() % (maxRoomSize - minRoomY) + minRoomY;
-            roomList[i]->cornerX = rand() % (floorMaxX - roomList[i]->sizeX - 2);
-            roomList[i]->cornerY = rand() % (floorMaxY - roomList[i]->sizeY - 2);
+            roomList[i]->sizeX = 4; // DEBUG
+            roomList[i]->sizeY = 4; // DEBUG
             
-            for (j = -1; j < roomList[i]->sizeX + 1; j++) {
+            temp1 = 4; // DEBUG
+            temp2 = 4; // DEBUG
+            //roomList[i]->sizeX = rand() % (maxRoomSize - minRoomX) + minRoomX;
+            //roomList[i]->sizeY = rand() % (maxRoomSize - minRoomY) + minRoomY;
+            roomList[i]->cornerX = rand() % (floorMaxX - temp1 - 2);
+            roomList[i]->cornerY = rand() % (floorMaxY - temp2 - 2);
+
+            /* for (j = -1; j < roomList[i]->sizeX + 1; j++) {
                 for (k = -1; k < roomList[i]->sizeY + 1; k++) {
                     if((*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type == roomChar || 
                         (*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type == edgeChar){
@@ -101,14 +107,41 @@ void roomGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomList)
                         (*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type = roomChar;
                     }
                 }
+            } */
+            // DEBUG vvv
+            for (j = -1; j < temp1 + 1; j++) {
+                for (k = -1; k < temp2 + 1; k++) {
+                    if((*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type == roomChar || 
+                        (*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type == edgeChar){
+                        placedAll = false;
+                    }
+                }
+            } 
+            
+            if(placedAll){
+                for (j = 0; j < temp1; j++) {
+                    for (k = 0; k < temp2; k++) {
+                        (*floor)[roomList[i]->cornerX + j][roomList[i]->cornerY + k].type = roomChar;
+                    }
+                }
             }
         }
+        // DEBUG ^^^
+        failCount++;
+        for(i = 0; i < roomsWanted; i++){
+            printf("(%2d, %2d) ", roomList[i]->sizeX, roomList[i]->sizeY);
+        }
+        printf("\n");
     }
-    //printf("%d\n", failCount);
+
+    for(i = 0; i < roomsWanted; i++){
+        printf("corner(X, Y)(%2d, %2d)  size(X, Y)(%2d, %2d)    Stored temps(X, Y)(%2d, %2d)\n", roomList[i]->cornerX, roomList[i]->cornerY, roomList[i]->sizeX, roomList[i]->sizeY, temp1, temp2);
+    }
+    printf("Failed: %d\n", failCount);
 }
 
 void staircaseGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomList)[maxRoomNumber], int roomsWanted) {
-    int i, ranX = 0, ranY = 0, ranX2 = 0, ranY2 = 0, ranDown, ranUp;
+    int i, ranX, ranY, ranX2, ranY2, ranDown, ranUp;
     
     int numStairs = ((rand() % roomsWanted) / 3);
     if(numStairs < 1 ){
@@ -136,8 +169,9 @@ void staircaseGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*room
 void corridorGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomList)[maxRoomNumber], int roomsWanted) 
 {
     int i, j, k, ranX, ranY, ranX2, ranY2;
-    //roomsWanted
+
     for (i = 0; i < roomsWanted; i++) {
+        //ranX = 0, ranY = 0, ranX2 = 0, ranY2 = 0;
         if(i < roomsWanted - 1) {
             ranX = roomList[i]->cornerX + (rand() % roomList[i]->sizeX);
             ranY = roomList[i]->cornerY + (rand() % roomList[i]->sizeY);
@@ -149,7 +183,7 @@ void corridorGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomL
             ranX2 = roomList[0]->cornerX + (rand() % roomList[0]->sizeX);
             ranY2 = roomList[0]->cornerY + (rand() % roomList[0]->sizeY);
         }
-
+        printf("ran1(X , Y)(%d , %d)  ran2(X, Y)(%d , %d)\n", ranX, ranY, ranX2, ranY2);
         int l = 0;
         for(j = 0; j < abs(ranX - ranX2); j++){
             if(ranX < ranX2){
@@ -182,33 +216,9 @@ void corridorGen(struct tiles(*floor)[floorMaxX][floorMaxY], struct rooms(*roomL
                 }
             }
         }
-
-        /////////////////////////
-        //Debug Version
-        /////////////////////////
-        //Debug Version
-        /////////////////////////
-        /* for(j = 0; j < abs(ranX - ranX2); j++){
-            if(ranX - ranX2 > 0){
-                    (*floor)[ranX + j][ranY].type = corridorChar;
-                    l++;
-            }
-            else{
-                    (*floor)[ranX - j][ranY].type = corridorChar;
-                    l--;
-            }
-        }
-        
-        for(k = 0; k < abs(ranY - ranY2); k++){
-            if(ranY - ranY2 > 0){
-                    (*floor)[ranX + l][ranY + k].type = corridorChar;
-            }
-            else {
-                    (*floor)[ranX + l][ranY - k].type = corridorChar;
-            }
-        }  */
     }
 }
+ 
 
 void placeBorder(struct tiles(*floor)[floorMaxX][floorMaxY])
 {
